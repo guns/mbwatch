66cb2014e1a6962548ee64d27a2f8bf62faa77aa
guns <self@sungpae.com>
2014-03-11 16:46:28 -0500

    Add process/interruptible-wait

--- src/mbwatch/process.clj
+++ src/mbwatch/process.clj
@@ -1,6 +1,7 @@
 (ns mbwatch.process
   (:require [clojure.java.io :as io]
             [mbwatch.types :refer [VOID]]
+            [mbwatch.util :refer [first-alt sig-wait]]
             [schema.core :as s :refer [enum]]))
 
 (s/defn spawn :- Process
@@ -15,6 +16,21 @@
     (with-open [out (.getOutputStream proc)]
       (when in (io/copy in out)))
     proc))
+
+(s/defn interruptible-wait :- Boolean
+  "Run process while waiting on signal. If a notification is received, the
+   process is terminated early and false is returned."
+  ([signal proc]
+   (interruptible-wait signal proc true))
+  ([signal    :- Object
+    proc      :- Process
+    graceful? :- Boolean]
+   (let [wait (future (sig-wait signal) false)]
+     (if (first-alt (.waitFor proc) (deref wait))
+       graceful?
+       (do (future-cancel wait)
+           (.destroy proc)
+           (recur signal proc false))))))
 
 (s/defn dump! :- VOID
   "Dump a process's stdout or stderr into writer."
--- test/mbwatch/process_test.clj
+++ test/mbwatch/process_test.clj
@@ -1,6 +1,7 @@
 (ns mbwatch.process-test
   (:require [clojure.test :refer [is]]
             [mbwatch.process :as p]
+            [mbwatch.util :refer [sig-notify-all]]
             [schema.test :as s]))
 
 (s/deftest test-spawn-and-dump!
@@ -11,3 +12,12 @@
            (p/dump! (p/spawn "sh" "-c" "echo Hello world. >&2") :err *out*))
          (with-out-str
            (p/dump! (p/spawn "cat" :in "Hello world.\n") :out *out*)))))
+
+(s/deftest test-interruptible-wait
+  (is (true? (p/interruptible-wait (Object.) (p/spawn "true"))))
+  (is (false?
+        (let [sig (Object.)
+              f (future (p/interruptible-wait sig (p/spawn "sleep" "1")))]
+          (Thread/sleep 10) ; Give the future thread a chance to start
+          (sig-notify-all sig)
+          @f))))
