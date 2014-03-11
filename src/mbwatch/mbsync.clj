17312e425f756a4a41b2d1e7238a913d69df9f3a
guns <self@sungpae.com>
2014-03-11 17:05:09 -0500

    Add interrupt on notify support to MbsyncWorker

--- src/mbwatch/mbsync.clj
+++ src/mbwatch/mbsync.clj
@@ -3,11 +3,13 @@
             [clojure.core.async.impl.protocols :refer [ReadPort WritePort]]
             [clojure.string :as string]
             [com.stuartsierra.component :refer [Lifecycle]]
-            [mbwatch.logging :refer [ERR ILogLevel INFO NOTICE]]
-            [mbwatch.process :as process :refer [dump!]]
+            [mbwatch.logging :refer [ERR ILogLevel INFO NOTICE WARNING]]
+            [mbwatch.process :as process :refer [dump! interruptible-wait]]
+            [mbwatch.types :refer [VOID]]
             [mbwatch.util :refer [poison-chan shell-escape thread-loop
                                   with-chan-value]]
-            [schema.core :as s :refer [maybe]])
+            [schema.core :as s :refer [maybe]]
+            [schema.utils :refer [class-schema]])
   (:import (org.joda.time DateTime)))
 
 (s/defn join-mbargs :- String
@@ -49,11 +51,14 @@
   ILogLevel
   (log-level [_] level))
 
+(declare sync-boxes!)
+
 (s/defrecord MbsyncWorker
-  [config   :- String
-   mbchan   :- String
-   req-chan :- ReadPort
-   log-chan :- WritePort]
+  [config           :- String
+   mbchan           :- String
+   req-chan         :- ReadPort
+   log-chan         :- WritePort
+   interrupt-signal :- Object]
 
   Lifecycle
 
@@ -62,28 +67,34 @@
     (assoc this ::worker
            (thread-loop []
              (with-chan-value [bs (<!! req-chan)]
-               (let [ev (strict-map->MbsyncEventStart
-                          {:level INFO
-                           :mbchan mbchan
-                           :mboxes bs
-                           :start (DateTime.)})
-                     proc (spawn-sync config mbchan bs)]
-                 (put! log-chan ev)
-
-                 (.waitFor proc)
-
-                 (let [v (.exitValue proc)
-                       ev' (strict-map->MbsyncEventStop
-                             (assoc ev
-                                    :level (if (zero? v) NOTICE ERR)
-                                    :stop (DateTime.)
-                                    :status v
-                                    :error (when-not (zero? v)
-                                             (with-out-str (dump! proc :err *out*)))))]
-                   (put! log-chan ev')))
+               (sync-boxes! this bs)
                (recur)))))
 
   (stop [this]
     (printf "↓ Stopping MbsyncWorker for channel `%s`\n" mbchan)
     (poison-chan req-chan (::worker this))
     (dissoc this ::worker)))
+
+(s/defn ^:private sync-boxes! :- VOID
+  [mbsync-worker-map :- (:schema (class-schema MbsyncWorker))
+   mboxes            :- [String]]
+  (let [{:keys [config mbchan req-chan log-chan interrupt-signal]} mbsync-worker-map
+        ev (strict-map->MbsyncEventStart
+             {:level INFO
+              :mbchan mbchan
+              :mboxes mboxes
+              :start (DateTime.)})
+        proc (spawn-sync config mbchan mboxes)
+        _ (put! log-chan ev)
+
+        graceful? (interruptible-wait interrupt-signal proc)
+
+        v (.exitValue proc)
+        ev' (strict-map->MbsyncEventStop
+              (assoc ev
+                     :level (if graceful? (if (zero? v) NOTICE ERR) WARNING)
+                     :stop (DateTime.)
+                     :status v
+                     :error (when-not (zero? v)
+                              (with-out-str (dump! proc :err *out*)))))]
+    (put! log-chan ev')))
