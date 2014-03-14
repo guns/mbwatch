08109b2c6ad703627e87edc60a641ae7f06270f8
guns <self@sungpae.com>
2014-03-14 18:24:30 -0500

    Implement Loggable for Components
    This allows us to simply stuff self down a logging channel!

--- src/mbwatch/logging.clj
+++ src/mbwatch/logging.clj
@@ -1,5 +1,5 @@
 (ns mbwatch.logging
-  (:require [clojure.core.async :refer [<!!]]
+  (:require [clojure.core.async :refer [<!! put!]]
             [clojure.core.async.impl.protocols :refer [ReadPort]]
             [com.stuartsierra.component :refer [Lifecycle]]
             [mbwatch.util :refer [class-name poison-chan thread-loop
@@ -56,7 +56,7 @@
   Lifecycle
 
   (start [this]
-    (printf "↑ Starting %s [%s %s]\n" (class-name this) (get log-levels level) (class-name logger))
+    (put! log-chan this)
     (assoc this :state-chan
            (thread-loop []
              (with-chan-value [obj (<!! log-chan)]
@@ -65,6 +65,18 @@
                (recur)))))
 
   (stop [this]
-    (printf "↓ Stopping %s [%s %s]\n" (class-name this) (get log-levels level) (class-name logger))
+    (put! log-chan this)
     (poison-chan log-chan state-chan)
-    (dissoc this :state-chan)))
+    (dissoc this :state-chan))
+
+  Loggable
+
+  (log-level [_] DEBUG)
+
+  (->log [this]
+    (let [msg (format "%s %s [%s %s]"
+                      (if state-chan "↓ Stopping" "↑ Starting")
+                      (class-name this)
+                      (get log-levels level)
+                      (class-name logger))]
+      (LogItem. DEBUG (DateTime.) msg))))
