8e9b4a658692a88bf4243b01bc7762b66be85c58
guns <self@sungpae.com>
2014-03-15 16:21:15 -0500

    Fix mbwatchrc test duplication

--- test/mbwatch/config/mbwatchrc_test.clj
+++ test/mbwatch/config/mbwatchrc_test.clj
@@ -1,7 +1,10 @@
-(ns mbwatch.config.mbwatch-test
+(ns mbwatch.config.mbwatchrc-test
   (:require [clojure.java.io :as io]
+            [clojure.test :refer [is]]
             [mbwatch.config.mbwatchrc :as mbw]
             [schema.test :as s]))
 
 (s/deftest test-parse
-  (mbw/parse (slurp (io/resource "test-resources/mbwatchrc"))))
+  (let [mbwatchrc (mbw/parse (slurp (io/resource "mbwatchrc")))]
+    (is (= (:notification-cmd mbwatchrc)
+           "notify - -a \"~/.sounds/new-message.mp3\""))))
