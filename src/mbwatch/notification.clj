a69a8cc559cdf6e8a86d897e4b4c9a2664c790ea
guns <self@sungpae.com>
2014-03-16 14:29:11 -0500

    Add notification ns with example maildirs

---
+++ src/mbwatch/notification.clj
@@ -0,0 +1,44 @@
+(ns mbwatch.notification
+  (:require [clojure.set :as set]
+            [clojure.string :as string]
+            [mbwatch.config :refer [mdir-path]]
+            [mbwatch.maildir :refer [new-messages senders]]
+            [mbwatch.mbsync]
+            [mbwatch.util :refer [to-ms]]
+            [schema.core :as s :refer [maybe]])
+  (:import (javax.mail.internet MimeMessage)
+           (mbwatch.mbsync MbsyncEventStop)))
+
+(s/defn ^:private format-msg :- (maybe String)
+  [messages :- [MimeMessage]]
+  (let [n (count messages)]
+    (when (pos? n)
+      (format "%d new message%s from:\n\n%s"
+              n
+              (if (= 1 n) "" \s)
+              (string/join \newline (senders messages))))))
+
+(s/defn ^:private sync-event->notification :- (maybe String)
+  [notify-map :- {String #{String}}
+   event      :- MbsyncEventStop]
+  (let [{:keys [mbchan mboxes maildir start]} event]
+    (when (and maildir (contains? notify-map mbchan))
+      (let [nboxes (notify-map mbchan)
+            bs (if (empty? mboxes)
+                 nboxes ; [] means full channel sync
+                 (set/intersection (set mboxes) nboxes))]
+        (when (seq bs)
+          (let [ts (to-ms start)]
+            (->> (sort bs)
+                 (mapv #(format "[%s/%s] %s"
+                                mbchan % (format-msg
+                                           (new-messages (mdir-path maildir %) ts))))
+                 (string/join "\n\n"))))))))
+
+(s/defn new-message-notification :- String
+  [notify-map :- {String #{String}}
+   events     :- [MbsyncEventStop]]
+  (->> events
+       (map (partial sync-event->notification notify-map))
+       (remove nil?)
+       (string/join "\n\n")))
---
+++ test/mbwatch/notification_test.clj
@@ -0,0 +1,48 @@
+(ns mbwatch.notification-test
+  (:require [clojure.test :refer [is]]
+            [mbwatch.logging :refer [DEBUG]]
+            [mbwatch.mbsync :as m]
+            [mbwatch.notification :as n]
+            [schema.test :as s])
+  (:import (org.joda.time DateTime)))
+
+(s/deftest test-new-message-notification
+  (is (= "[FOO-chan/INBOX] 2 new messages from:\n\nAlice <alice@example.com>\n★ ❤ Carol ❤ ★ <carol@example.com>"
+         (n/new-message-notification
+           {"FOO-chan" #{"INBOX"}}
+           [(m/strict-map->MbsyncEventStop
+              {:level DEBUG
+               :mbchan "FOO-chan"
+               :mboxes []
+               :start (DateTime. 0)
+               :stop (DateTime.)
+               :status 0
+               :error nil
+               :maildir {:inbox "test-resources/maildir/foo-mdir/INBOX"
+                         :path "test-resources/maildir/foo-mdir/"
+                         :flatten nil}})
+            (m/strict-map->MbsyncEventStop
+              {:level DEBUG
+               :mbchan "BAR-chan"
+               :mboxes []
+               :start (DateTime. 0)
+               :stop (DateTime.)
+               :status 0
+               :error nil
+               :maildir {:inbox "test-resources/maildir/bar-mdir/INBOX"
+                         :path "test-resources/maildir/bar-mdir/"
+                         :flatten nil}})])))
+  (is (re-find #"\A\[FOO-chan/clojure\] 17 new messages from:"
+               (n/new-message-notification
+                 {"FOO-chan" #{"clojure"}}
+                 [(m/strict-map->MbsyncEventStop
+                    {:level DEBUG
+                     :mbchan "FOO-chan"
+                     :mboxes []
+                     :start (DateTime. 0)
+                     :stop (DateTime.)
+                     :status 0
+                     :error nil
+                     :maildir {:inbox "test-resources/maildir/foo-mdir/INBOX"
+                               :path "test-resources/maildir/foo-mdir/"
+                               :flatten nil}})]))))
