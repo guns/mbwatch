d9a728e44cf870244000098f52098c9ce16a0569
guns <self@sungpae.com>
2014-04-05 13:05:33 -0500

    Add ConnectionEvent and attach watcher to the conn atom

--- src/mbwatch/connection.clj
+++ src/mbwatch/connection.clj
@@ -1,16 +1,33 @@
 (ns mbwatch.connection
-  (:require [clojure.core.async :refer [<!! close! put!]]
+  (:require [clojure.core.async :refer [<!! close!]]
             [clojure.core.async.impl.protocols :refer [ReadPort WritePort]]
             [com.stuartsierra.component :refer [Lifecycle]]
             [mbwatch.concurrent :refer [sig-notify-all sig-wait thread-loop]]
             [mbwatch.config.mbsyncrc :refer [IMAPCredential]]
-            [mbwatch.logging :refer [->LogItem DEBUG Loggable log!]]
+            [mbwatch.logging :refer [->LogItem DEBUG Loggable NOTICE WARNING
+                                     log!]]
             [mbwatch.types :as t :refer [Word]]
             [mbwatch.util :refer [catch-print]]
             [schema.core :as s :refer [Int maybe protocol]])
   (:import (clojure.lang Atom)
            (java.util.concurrent Future)
            (java.util.concurrent.atomic AtomicBoolean AtomicLong)))
+
+(t/defrecord ^:private ConnectionEvent
+  [mbchan :- String
+   status :- Boolean]
+
+  Loggable
+
+  (log-level [_] (if status NOTICE WARNING))
+
+  (log-item [this]
+    (let [msg (str "Channel " mbchan " → " (if status "reachable" "unreachable"))]
+      (->LogItem this msg))))
+
+(s/defn update-connections! :- {String Boolean}
+  [connection-atom        :- Atom
+   mbchan->IMAPCredential :- {Word IMAPCredential}])
 
 (t/defrecord ^:private ConnectionWatcher
   [mbchan->IMAPCredential :- {Word IMAPCredential}
@@ -27,13 +44,19 @@
 
   (start [this]
     (log! log-chan this)
+    (add-watch connection-atom ::log-conn-changes
+               (fn [_ _ o n]
+                 (doseq [[mbchan conn] n
+                         :let [oconn (o mbchan)]]
+                   (when-not (= oconn conn)
+                     (log! log-chan (ConnectionEvent. mbchan conn))))))
     (assoc this
            :exit-future
            (future
              (catch-print
                (loop []
                  (when (.get status)
-                   (put! log-chan "UPDATE CONNECTION STATUS HERE")
+                   ; (update-connections! connection-atom mbchan->IMAPCredential)
                    (let [poll (.get poll-ms)]
                      (.set next-check (+ (System/currentTimeMillis) poll))
                      (sig-wait status poll)
@@ -54,6 +77,7 @@
     (log! log-chan this)
     (sig-notify-all status)
     (close! cmd-chan)
+    (remove-watch connection-atom ::log-conn-changes)
     @exit-future
     (<!! exit-chan)
     (dissoc this :exit-chan :exit-future))
