443c80c9289fde759a8af87cffb06553bff1aad6
guns <self@sungpae.com>
2014-04-09 18:37:58 -0500

    Refactor connection-watcher tests

--- test/mbwatch/connection_watcher_test.clj
+++ test/mbwatch/connection_watcher_test.clj
@@ -3,53 +3,55 @@
             [schema.test :refer [deftest]]))
 
 (deftest test-private-behavior
-  (let [merge-pending-syncs #'mbwatch.connection-watcher/merge-pending-syncs]
-    (is (= (merge-pending-syncs {"a" {:status false :pending-syncs nil}}
-                                {"a" ["INBOX"]})
+  (let [tfn #'mbwatch.connection-watcher/merge-pending-syncs]
+    (is (= (tfn {"a" {:status false :pending-syncs nil}}
+                {"a" ["INBOX"]})
            [{"a" {:status false :pending-syncs #{"INBOX"}}} {}]))
-    (is (= (merge-pending-syncs {"a" {:status false :pending-syncs #{"INBOX"}}}
-                                {"a" ["work"]})
+    (is (= (tfn {"a" {:status false :pending-syncs #{"INBOX"}}}
+                {"a" ["work"]})
            [{"a" {:status false :pending-syncs #{"INBOX" "work"}}} {}]))
-    (is (= (merge-pending-syncs {"a" {:status false :pending-syncs ^:all-mboxes #{}}}
-                                {"a" ["work"]})
+    (is (= (tfn {"a" {:status false :pending-syncs ^:all-mboxes #{}}}
+                {"a" ["work"]})
            [{"a" {:status false :pending-syncs ^:all-mboxes #{}}} {}]))
-    (is (= (merge-pending-syncs {"a" {:status true :pending-syncs #{"INBOX"}}}
-                                {"a" ["work"]})
+    (is (= (tfn {"a" {:status true :pending-syncs #{"INBOX"}}}
+                {"a" ["work"]})
            [{"a" {:status true :pending-syncs #{"INBOX"}}}
             {"a" ["work"]}])))
-  (let [update-connections-for-sync #'mbwatch.connection-watcher/update-connections-for-sync
-        vfn (juxt identity (comp :mbwatch.connection-watcher/sync-req meta))
+  (let [tfn (fn [& args]
+              (-> #'mbwatch.connection-watcher/update-connections-for-sync
+                  (apply args)
+                  ((juxt identity (comp :mbwatch.connection-watcher/sync-req meta)))))
         up {:host "example.com" :port 80 :user "test" :pass "test"}
         down {:host "IDENT-PORT.example.com" :port 113 :user "test" :pass "test"}
         all-up {"a" up "b" up}
         all-down {"a" down "b" down}
         mixed {"a" up "b" down}]
-    (is (= (vfn (update-connections-for-sync {"a" {:status true :pending-syncs nil}
-                                              "b" {:status true :pending-syncs nil}}
-                                             {"a" ["INBOX"]}
-                                             all-down))
+    (is (= (tfn {"a" {:status true :pending-syncs nil}
+                 "b" {:status true :pending-syncs nil}}
+                {"a" ["INBOX"]}
+                all-down 0)
            [{"a" {:status false :pending-syncs #{"INBOX"}}
              "b" {:status true :pending-syncs nil}}
             {}]))
-    (is (= (vfn (update-connections-for-sync {"a" {:status false :pending-syncs #{"INBOX"}}
-                                              "b" {:status true :pending-syncs nil}}
-                                             {"a" ["work"]}
-                                             all-down))
+    (is (= (tfn {"a" {:status false :pending-syncs #{"INBOX"}}
+                 "b" {:status true :pending-syncs nil}}
+                {"a" ["work"]}
+                all-down 0)
            [{"a" {:status false :pending-syncs #{"INBOX" "work"}}
              "b" {:status true :pending-syncs nil}}
             {}]))
-    (is (= (vfn (update-connections-for-sync {"a" {:status false :pending-syncs nil}
-                                              "b" {:status false :pending-syncs nil}}
-                                             {"a" ["INBOX"] "b" ["INBOX"]}
-                                             all-up))
+    (is (= (tfn {"a" {:status false :pending-syncs nil}
+                 "b" {:status false :pending-syncs nil}}
+                {"a" ["INBOX"] "b" ["INBOX"]}
+                all-up 2000)
            [{"a" {:status true :pending-syncs #{"INBOX"}}
              "b" {:status true :pending-syncs #{"INBOX"}}}
             ;; The watch-fn is responsible for releasing these syncs
             {}]))
-    (is (= (vfn (update-connections-for-sync {"a" {:status true :pending-syncs nil}
-                                              "b" {:status true :pending-syncs nil}}
-                                             {"a" ["INBOX"] "b" ["INBOX"]}
-                                             mixed))
+    (is (= (tfn {"a" {:status true :pending-syncs nil}
+                 "b" {:status true :pending-syncs nil}}
+                {"a" ["INBOX"] "b" ["INBOX"]}
+                mixed 2000)
            [{"a" {:status true :pending-syncs nil}
              "b" {:status false :pending-syncs #{"INBOX"}}}
             {"a" ["INBOX"]}]))))
