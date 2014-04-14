3f9641e89787a35b388c78196898a9134fcd30ea
guns <self@sungpae.com>
2014-04-13 22:23:24 -0500

    Add test.common
    This should have been added ~2 commits ago

--- test/mbwatch/sync_timer_test.clj
+++ test/mbwatch/sync_timer_test.clj
@@ -1,51 +1,30 @@
 (ns mbwatch.sync-timer-test
-  (:require [clojure.core.async :refer [<!! chan close!]]
+  (:require [clojure.core.async :refer [chan close!]]
             [clojure.test :refer [is]]
             [clojure.test.check.clojure-test :refer [defspec]]
-            [clojure.test.check.generators :as g :refer [elements fmap one-of
-                                                         return such-that
+            [clojure.test.check.generators :as g :refer [fmap such-that
                                                          tuple]]
             [clojure.test.check.properties :refer [for-all]]
             [com.stuartsierra.component :as comp]
-            [mbwatch.command :refer [->Command]]
+            [mbwatch.command]
             [mbwatch.concurrent :refer [update-timer!]]
-            [mbwatch.sync-timer :as st])
+            [mbwatch.sync-timer :as st]
+            [mbwatch.test.common :refer [chanv sync-request-gen]])
   (:import (mbwatch.command Command)
            (mbwatch.sync_timer SyncTimer)))
 
-(def mbox-gen
-  (elements (mapv (comp str char) (range (int \α) (inc (int \ω))))))
-
-(def mbchan-gen
-  (elements (mapv str '[alpha beta gamma delta epsilon zeta eta theta iota
-                        kappa lambda mu nu xi omicron pi rho sigma tau upsilon
-                        phi chi psi omega])))
-
-(def sync-request-gen
-  (->> (tuple mbchan-gen (g/vector mbox-gen))
-       g/vector
-       (fmap (partial into {}))))
-
-(def command-gen
-  (one-of [(fmap #(->Command :sync %) sync-request-gen)
-           (fmap #(->Command :sync/term %) (return nil))
-           (fmap #(->Command :conn/trigger %) (return nil))
-           (fmap #(->Command :conn/set-period %) g/int)
-           (fmap #(->Command :conn/remove %) (g/vector mbchan-gen))
-           (fmap #(->Command :notify/add %) sync-request-gen)
-           (fmap #(->Command :notify/remove %) sync-request-gen)
-           (fmap #(->Command :notify/set %) sync-request-gen)
-           (fmap #(->Command :timer/trigger %) (return nil))
-           (fmap #(->Command :timer/set-period %) g/int)
-           (fmap #(->Command :timer/set-request %) sync-request-gen)]))
-
 (def sync-timer-gen
-  (->> (tuple sync-request-gen g/int)
+  (->> (tuple sync-request-gen g/nat)
        (fmap (fn [[req n]] (st/->SyncTimer req (chan 0x10000) n)))))
 
-(defn chanv [ch]
-  (into [] (take-while some? (repeatedly #(<!! ch)))))
-
+; (defn =*
+;   "Equal to, within tolerances."
+;   [state state']
+;   (if state'
+;     (and (is (= (:sync-request state') (:sync-request state)))
+;          (is (= (:period state') (:period state)))
+;          (is (<= 0 (- (:alarm state') (:alarm state)) 10)))
+;     (is (= state' state))))
 
 (defspec test-cyclic-timer 10
   (for-all [sync-timer (such-that #(seq @(:sync-request-atom %)) sync-timer-gen)
@@ -70,3 +49,34 @@
              ;; Lifecycle events only
              (is (= (mapv class (chanv log-chan)) [SyncTimer SyncTimer])
                  "only logs itself"))))))
+
+; (defspec test-sync-timer-commands 100
+;   (for-all [sync-timer sync-timer-gen
+;             input-cmds (g/vector command-gen)]
+;     (let [{:keys [cmd-chan-in cmd-chan-out log-chan sync-request-atom timer-atom]} sync-timer
+;           sync-req-states (atom [@sync-request-atom])
+;           timer-states (atom [@timer-atom])
+;           _ (do (add-watch sync-request-atom ::watcher (fn [_ _ _ n] (swap! sync-req-states conj n)))
+;                 (add-watch timer-atom ::watcher (fn [_ _ o n] (swap! timer-states conj n))))
+;           ;; Spam the command channel
+;           f (future (doseq [cmd input-cmds] (>!! cmd-chan-in cmd)))
+;           sync-timer (comp/start sync-timer)
+;           ;; Settle down
+;           _ (do @f
+;                 (Thread/sleep 10)
+;                 (close! cmd-chan-in)
+;                 (comp/stop sync-timer)
+;                 (remove-watch sync-request-atom ::watcher)
+;                 (remove-watch timer-atom ::watcher))
+;           ;; Construct a hypothetical chain of commands given our state snapshots
+;           _ (.println System/err "─────────────────────────────────────────────────────────────")
+;           hyp (reduce
+;                 (fn [v [o n]]
+;                   (.println System/err [o '-> n])
+;                   (cond (not= (:period o) (:period n)) (conj v [:timer/set-period (:period n)])
+;                         (not= (:alarm o) (:alarm n)) (conj v [:timer/trigger nil])
+;                         :else v))
+;                 [] (partition 2 1 @timer-states))]
+;       (.println System/err hyp)
+;       (.println System/err (mapv (juxt :opcode :payload) input-cmds))
+;       true)))
---
+++ test/mbwatch/test/common.clj
@@ -0,0 +1,41 @@
+(ns mbwatch.test.common
+  (:require [clojure.core.async :refer [<!! chan]]
+            [clojure.test.check.generators :as g :refer [elements fmap one-of
+                                                         return tuple]]
+            [mbwatch.command :refer [->Command]]
+            [mbwatch.sync-timer :as st]))
+
+(def mbox-gen
+  (elements (mapv (comp str char) (range (int \α) (inc (int \ω))))))
+
+(def mbchan-gen
+  (elements (mapv str '[alpha beta gamma delta epsilon zeta eta theta iota
+                        kappa lambda mu nu xi omicron pi rho sigma tau upsilon
+                        phi chi psi omega])))
+
+(def sync-request-gen
+  (->> (tuple mbchan-gen (g/vector mbox-gen))
+       g/vector
+       (fmap (partial into {}))))
+
+(def command-gen
+  (one-of [(fmap #(->Command :sync %) sync-request-gen)
+           (fmap #(->Command :sync/term %) (return nil))
+           (fmap #(->Command :conn/trigger %) (return nil))
+           (fmap #(->Command :conn/set-period %) g/int)
+           (fmap #(->Command :conn/remove %) (g/vector mbchan-gen))
+           (fmap #(->Command :notify/add %) sync-request-gen)
+           (fmap #(->Command :notify/remove %) sync-request-gen)
+           (fmap #(->Command :notify/set %) sync-request-gen)
+           (fmap #(->Command :timer/trigger %) (return nil))
+           (fmap #(->Command :timer/set-period %) g/int)
+           (fmap #(->Command :timer/set-request %) sync-request-gen)]))
+
+(defn chanv [ch]
+  (into [] (take-while some? (repeatedly #(<!! ch)))))
+
+(defn tol?
+  "Is x within the expected deviation of k due to concurrent scheduling and
+   test-time schema validation?"
+  [k x]
+  (<= -0.05 (double (/ (- x k) k)) 0.05))
