cc37f21ed5fe7c16567ccf6f1d33be33d5a8078b
guns <self@sungpae.com>
2014-04-14 11:40:19 -0500

    Put the test-sync-timer-commands property test on the shelf

--- test/mbwatch/sync_timer_test.clj
+++ test/mbwatch/sync_timer_test.clj
@@ -12,15 +12,6 @@
   (:import (mbwatch.command Command)
            (mbwatch.sync_timer SyncTimer)))
 
-; (defn =*
-;   "Equal to, within tolerances."
-;   [state state']
-;   (if state'
-;     (and (is (= (:sync-request state') (:sync-request state)))
-;          (is (= (:period state') (:period state)))
-;          (is (<= 0 (- (:alarm state') (:alarm state)) 10)))
-;     (is (= state' state))))
-
 (defspec test-cyclic-timer 10
   (for-all [sync-req (such-that seq sync-request-gen)
             ttl (g/choose 100 1000)]
@@ -46,33 +37,52 @@
              (is (= (mapv class (chanv log-chan)) [SyncTimer SyncTimer])
                  "only logs itself"))))))
 
+; (defn trigger?
+;   "Command is either a :timer/trigger or a NOP :timer/set-period?"
+;   [cur-period cmd]
+;   (let [{:keys [opcode payload]} cmd]
+;     (is (case opcode
+;           :timer/trigger true
+;           :timer/set-period (if (zero? cur-period)
+;                               (< payload MIN-POS-PERIOD)
+;                               (= payload cur-period))
+;           false))))
+
+; (defn cmd= [opcode payload command]
+;   (is (= opcode (:opcode command)))
+;   (is (= payload (zero-or-min (:payload command) MIN-POS-PERIOD))))
+
 ; (defspec test-sync-timer-commands 100
-;   (for-all [sync-timer sync-timer-gen
-;             input-cmds (g/vector command-gen)]
-;     (let [{:keys [cmd-chan-in cmd-chan-out log-chan sync-request-atom timer-atom]} sync-timer
+;   (for-all [sync-req (no-shrink sync-request-gen)
+;             input-cmds (no-shrink (g/vector command-gen))]
+;     (.println System/err "─────────────────────────────────────────────────────────────")
+;     (let [sync-timer (->SyncTimer sync-req (chan) 0)
+;           {:keys [cmd-chan-in cmd-chan-out log-chan sync-request-atom timer-atom]} sync-timer
 ;           sync-req-states (atom [@sync-request-atom])
 ;           timer-states (atom [@timer-atom])
 ;           _ (do (add-watch sync-request-atom ::watcher (fn [_ _ _ n] (swap! sync-req-states conj n)))
 ;                 (add-watch timer-atom ::watcher (fn [_ _ o n] (swap! timer-states conj n))))
-;           ;; Spam the command channel
-;           f (future (doseq [cmd input-cmds] (>!! cmd-chan-in cmd)))
+;           ;; Pipe in our commands without spamming (multiple :timer/trigger
+;           ;; commands would otherwise go unnoticed)
+;           f (future (doseq [cmd input-cmds]
+;                       (>!! cmd-chan-in cmd)
+;                       (Thread/sleep 10)))
 ;           sync-timer (comp/start sync-timer)
 ;           ;; Settle down
 ;           _ (do @f
-;                 (Thread/sleep 10)
 ;                 (close! cmd-chan-in)
 ;                 (comp/stop sync-timer)
 ;                 (remove-watch sync-request-atom ::watcher)
 ;                 (remove-watch timer-atom ::watcher))
-;           ;; Construct a hypothetical chain of commands given our state snapshots
-;           _ (.println System/err "─────────────────────────────────────────────────────────────")
-;           hyp (reduce
-;                 (fn [v [o n]]
-;                   (.println System/err [o '-> n])
-;                   (cond (not= (:period o) (:period n)) (conj v [:timer/set-period (:period n)])
-;                         (not= (:alarm o) (:alarm n)) (conj v [:timer/trigger nil])
-;                         :else v))
-;                 [] (partition 2 1 @timer-states))]
-;       (.println System/err hyp)
-;       (.println System/err (mapv (juxt :opcode :payload) input-cmds))
-;       true)))
+;           ;; Construct a hypothetical chain of command predicates given our
+;           ;; state snapshots
+;           preds (reduce
+;                   (fn [v [o n]]
+;                     (.println System/err [o '-> n])
+;                     (cond (not= (:period o) (:period n)) (conj v (partial cmd= :timer/set-period (:period n)))
+;                           :else (conj v (partial trigger? (:period n)))))
+;                   [] (partition 2 1 @timer-states))]
+;       (.println System/err (mapv (juxt :id :opcode :payload) input-cmds))
+;       ;; Compare our expected commands against the actual input
+;       (is (= (count preds) (count input-cmds)))
+;       (is (every? true? (mapv (fn [p cmd] (p cmd)) preds input-cmds))))))
