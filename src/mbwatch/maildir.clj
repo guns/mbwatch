9a6b546332ff7d3b7049023ff0484d745ff2b2fb
guns <self@sungpae.com>
2014-04-28 22:36:30 -0500

    Add maildir/get-all-mdirs

--- src/mbwatch/maildir.clj
+++ src/mbwatch/maildir.clj
@@ -1,5 +1,6 @@
 (ns mbwatch.maildir
   (:require [clojure.java.io :as io :refer [Coercions]]
+            [clojure.set :refer [subset?]]
             [clojure.string :as string]
             [mbwatch.config.mbsyncrc :refer [Maildirstore]]
             [schema.core :as s])
@@ -12,6 +13,11 @@
   (MimeMessage. (Session/getDefaultInstance (System/getProperties))
                 (FileInputStream. file)))
 
+(s/defn ^:private maildir? :- Boolean
+  [f :- File]
+  (and (.isDirectory f)
+       (subset? #{"cur" "new" "tmp"} (set (.list f)))))
+
 (s/defn get-mdir :- File
   [maildir :- Maildirstore
    mbox    :- String]
@@ -21,6 +27,17 @@
           :else (->> (string/split mbox #"/")
                      (string/join flatten)
                      (io/file path)))))
+
+(s/defn get-all-mdirs :- #{File}
+  [maildir :- Maildirstore]
+  (let [{:keys [inbox path]} maildir
+        inbox-dir (io/file inbox)
+        mdirs (cond-> #{}
+                (maildir? inbox-dir) (conj inbox-dir))]
+    (->> (io/file path)
+         .listFiles
+         (filterv (fn [^File f] (and (.isDirectory f) (maildir? f))))
+         (into mdirs))))
 
 (s/defn new-messages :- [MimeMessage]
   "Vector of new messages in maildir newer than given timestamp. Messages are
--- test/mbwatch/maildir_test.clj
+++ test/mbwatch/maildir_test.clj
@@ -1,7 +1,8 @@
 (ns mbwatch.maildir-test
   (:require [clojure.java.io :as io]
             [clojure.test :refer [is]]
-            [mbwatch.maildir :refer [get-mdir new-messages senders]]
+            [mbwatch.maildir :refer [get-all-mdirs get-mdir new-messages
+                                     senders]]
             [schema.test :refer [deftest]])
   (:import (java.io File)))
 
@@ -21,9 +22,17 @@
     (is (= (get-mdir (assoc maildir :flatten nil) "[Gmail]/Sent Mail")
            (io/file "/home/user/Mail/gmail/[Gmail]/Sent Mail")))))
 
+(deftest test-get-all-mdirs
+  (is (= (into #{} (mapv #(io/file "test-resources/maildir/foo-mdir" %) ["INBOX" "clojure"]))
+         (get-all-mdirs {:path "test-resources/maildir/foo-mdir"
+                         :inbox "test-resources/maildir/foo-mdir/INBOX"
+                         :flatten nil}))))
+
 (deftest test-new-messages
   (is (= (count (new-messages TEST-MDIR 0))
-         (count (filterv #(.isFile ^File %) (file-seq (File. TEST-MDIR)))))))
+         (count (filterv (fn [^File f]
+                           (and (.isFile f) (re-find #"\A[^.]" (.getName f))))
+                         (file-seq (File. TEST-MDIR)))))))
 
 (deftest test-senders
   (is (= (senders (new-messages TEST-MDIR 0))
